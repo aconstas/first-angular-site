18a72560a215c8c723a27c8351b6cc863a6edb63
aconstas <82304192+aconstas@users.noreply.github.com>
2023-11-07 17:13:02 -0800

    Import SharedModule
    Contains Pipes and resources shared across app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { CustomersModule } from './customers/customers.module';
 import { AppComponent }  from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  imports:      [ BrowserModule, CustomersModule ],
+  // Our root 'bucket' now knows about 3 imported modules
+  imports:      [ BrowserModule, CustomersModule, SharedModule],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
