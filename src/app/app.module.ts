196de5bc9f4a2e1a8e04571aad8253ecb6841a9a
aconstas <82304192+aconstas@users.noreply.github.com>
2023-11-09 15:01:43 -0800

    import CoreModule, AppRoutingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CoreModule } from './core/core.module';
 import { CustomersModule } from './customers/customers.module';
 import { AppComponent }  from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  // Our root 'bucket' now knows about 3 imported modules
-  imports:      [ BrowserModule, CustomersModule, SharedModule],
+  // Our root 'bucket' now knows about 4 imported modules
+  imports:      [ BrowserModule, CoreModule, CustomersModule, SharedModule, AppRoutingModule],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
