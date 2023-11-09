911f8a72dcb71b90bdf2d2329deb1dad3de81979
aconstas <82304192+aconstas@users.noreply.github.com>
2023-11-09 11:08:41 -0800

    import FormsModule

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { SharedModule } from '../shared/shared.module';
@@ -7,8 +8,12 @@ import { CustomersListComponent } from './customers-list/customers-list.componen
 import { FilterTextboxComponent } from './customers-list/filter-textbox.component';
 
 @NgModule({
-  imports: [CommonModule, SharedModule],
-  declarations: [CustomersComponent, CustomersListComponent, FilterTextboxComponent],
+  imports: [CommonModule, SharedModule, FormsModule],
+  declarations: [
+    CustomersComponent,
+    CustomersListComponent,
+    FilterTextboxComponent,
+  ],
   exports: [CustomersComponent],
 })
 export class CustomersModule {}
