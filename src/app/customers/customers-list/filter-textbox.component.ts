504a476f2383375cc2f24a9d5a96a528ae5dbde3
aconstas <82304192+aconstas@users.noreply.github.com>
2023-11-08 20:43:08 -0800

    add input & output for filtering functionality

--- src/app/customers/customers-list/filter-textbox.component.ts
+++ src/app/customers/customers-list/filter-textbox.component.ts
@@ -1,13 +1,29 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
     selector: 'filter-textbox',
     template: `
-        Filter: <input type="text" />
+        Filter: <input type="text" [(ngModel)]="filter" />
     `
 })
 
 export class FilterTextboxComponent implements OnInit {
+    
+    private _filter: string;
+    @Input() get filter() {
+        return this._filter;
+    }
+    
+    set filter(val: string) { 
+        this._filter = val;
+        this.changed.emit(this.filter); //Raise changed event
+    }
+
+    // 'changed' is of type EventEmitter
+    // EventEmitter is a way in Angular to send data up to parent from a child
+    // in this case we're sending up <string> data, a single value
+    @Output() changed: EventEmitter<string> = new EventEmitter<string>();
+    // EventEmitter<string> is a TypeScript 'generic'
     constructor() {}
 
     ngOnInit(): void {
